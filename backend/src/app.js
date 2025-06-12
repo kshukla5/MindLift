929c6cdf9aee59782a763c717ca1711c44491328
skywardcloud <info@skywardtechno.com>
2025-06-12 13:13:51 -0400

    Add Stripe subscription route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const speakerRoutes = require('./routes/speakerRoutes');
 const videoRoutes = require('./routes/videoRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/api', userRoutes);
 app.use('/api', speakerRoutes);
 app.use('/api', videoRoutes);
+app.use('/api', paymentRoutes);
 
 module.exports = app;
