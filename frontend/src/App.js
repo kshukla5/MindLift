ac40a82249d9726ca5c77b3561e0e5b755479883
skywardcloud <info@skywardtechno.com>
2025-06-23 18:16:04 -0400

    Remove unused login/signup forms and update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import AboutUs from './components/AboutUs';
-import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignupForm';
+import Login from './components/Login';
+import Signup from './components/Signup';
 import FeaturesPage from './components/FeaturesPage';
 import ContactPage from './components/ContactPage';
 import Footer from './components/Footer';
@@ -19,8 +19,8 @@ function App() {
           <Routes>
             <Route path="/" element={<HeroSection />} />
             <Route path="/about" element={<AboutUs />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/features" element={<FeaturesPage />} />
             <Route path="/contact" element={<ContactPage />} />
           </Routes>
